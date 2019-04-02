9566169b743aff229622c3292af5aa6ba48c1bee
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-04-02 10:21:09 +0700

    add Series model

---
+++ viz-dev-backend/database/migrations/2019_04_02_023846_create_series_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description', 400);
+            $table->integer('start_year');
+            $table->integer('end_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('series');
+    }
+}
