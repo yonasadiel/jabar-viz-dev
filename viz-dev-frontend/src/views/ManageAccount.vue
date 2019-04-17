75dbc740b532be1396a3b3048da5cc3609973d8e
adamfdhl <adamfdhl@gmail.com>
2019-04-17 11:58:59 +0700

    test manage account page

---
+++ viz-dev-frontend/src/views/ManageAccount.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <h1>Jabar Viz Dev Account Management</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'manage account',
+};
+</script>
+
+<style scoped>
+
+</style>
