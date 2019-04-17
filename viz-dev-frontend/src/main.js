4ffadf7b3ed6338fe7eec08effbf1f2556878630
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-04-17 12:35:08 +0700

    delete hello world and remove warnings

--- viz-dev-frontend/src/main.js
+++ viz-dev-frontend/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar.vue';
 
 Vue.component('Navbar', Navbar);
 Vue.config.productionTip = false;
