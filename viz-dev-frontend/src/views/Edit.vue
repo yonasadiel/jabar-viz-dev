f30c265f5be2eb731fd443bf682d6f6db7136e29
diasakbar <13516144@std.stei.itb.ac.id>
2019-04-20 04:39:56 +0700

    Modified Edit & Edit_Page

--- viz-dev-frontend/src/views/Edit.vue
+++ viz-dev-frontend/src/views/Edit.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h1>Edit Page</h1>
+    <Navbar></Navbar>
+    <div class="container">
+        <h1 class="title">JABAR DATA VISUALIZATION EDITING CENTER</h1>
+        <div class="container__data">
+            <div class="btn__data" v-for="content in contents" @click="redirectPage(content.name)">
+              {{ content.name }}
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -9,9 +17,79 @@ export default {
   name: 'Edit',
   components: {
   },
+  data(){
+    return{
+      contents: [
+        {name: 'Harapan Lama Sekolah'},
+        {name: 'Rata-Rata Lama Sekolah'},
+        {name: 'IPM'},
+        {name: 'Jumlah Penduduk Miskin'},
+        {name: 'Persentase Penduduk Miskin'},
+        {name: 'Indeks Kedalaman Kemiskinan'},
+        {name: 'Indeks Keparahan Kemiskinan'},
+        {name: 'Garis Kemiskinan'},
+        {name: 'Gini Ratio'},
+        {name: 'Jumlah Penduduk'},
+        {name: 'Laju Pertumbuhan'},
+        {name: 'Kepadatan Penduduk'},
+        {name: 'Rumah Sakit'},
+        {name: 'Puskesmas'},
+        {name: 'Petugas Medis'},
+        {name: 'Dokter'},
+        {name: 'Bidan'},
+        {name: 'Jumlah SD Negeri/Swasta'},
+        {name: 'Jumlah SLTP Negeri/Swasta'},
+        {name: 'Jumlah SLTA Negeri/Swasta'},
+        {name: 'Jumlah SMK Negeri/Swasta'},
+        {name: 'Tingkat Partisipasi Angkatan Kerja P/L'},
+        {name: 'Tingkat Pengangguran Terbuka (P/L) (Jiwa)'},
+        {name: 'Industri Besar'},
+        {name: 'Industri Kecil'},
+      ]
+    }
+  },
+  methods: {
+    redirectPage: function (content) {
+      this.$router.push({path:'edit_data', query: { data: content }});
+    }
+  }
 };
 </script>
 
 <style scoped>
+.title {
+    text-align: center;
+}
+.container {
+    padding: 40px 76px;
+    text-align: center;
+}
+
+.container__data {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 40px 0;
+}
+
+.btn__data {
+    background-color: #0e76bf;
+    color: white;
+    font-size: 1.2em;
+    border-radius: 5px;
+    margin: 10px;
+    width: 125px;
+    padding: 16px;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: transform .5s ease;
+    transform: scale(1);
+    box-shadow: 2px 2px 8px 2px rgba(0, 0, 0, .2);
+}
+
 
 </style>
--- viz-dev-frontend/src/views/EditData.vue
+++ viz-dev-frontend/src/views/EditData.vue
@@ -1,6 +1,39 @@
 <template>
   <div>
-    <h1>Edit Data Page</h1>
+    <Navbar></Navbar>
+    <div class="container">
+        <div class="header">
+            <h1>JABAR DATA VISUALIZATION</h1>
+            <h2>{{this.$route.query.data}}</h2>
+        </div>
+        <div class="container__edit">
+            <div class="flex-row">
+                <div class="flex-col col-header">
+                    <div class="row-header">Kab / Kota</div>
+                    <div class="row-entry" v-for="kota in kab_kota">{{kota.name}}</div>
+                </div>
+                <div class="flex-col col-entry">
+                    <div class="row-header">2016</div>
+                    <div class="row-entry"><input type="text" class="number-entry" value="1.2"></div>
+                    <div class="row-entry"><input type="text" class="number-entry" value="1.3"></div>
+                    <div class="row-entry"><input type="text" class="number-entry" value="1.4"></div>
+                </div>
+                <div class="flex-col col-entry">
+                    <div class="row-header">2017</div>
+                    <div class="row-entry"><input type="text" class="number-entry" value="1.2"></div>
+                    <div class="row-entry"><input type="text" class="number-entry" value="1.3"></div>
+                    <div class="row-entry"><input type="text" class="number-entry" value="1.4"></div>
+                </div>
+                <div class="flex-col col-entry">
+                    <div class="row-header">2018</div>
+                    <div class="row-entry"><input type="text" class="number-entry" value="1.2"></div>
+                    <div class="row-entry"><input type="text" class="number-entry" value="1.3"></div>
+                    <div class="row-entry"><input type="text" class="number-entry" value="1.4"></div>
+                </div>
+            </div>
+            <button class="btn__save">SAVE</button>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -9,9 +42,119 @@ export default {
   name: 'Edit_Data',
   components: {
   },
+  data(){
+    return{
+      kab_kota: [
+        {name: 'Kota Bandung',},
+        {name: 'Kota Bekasi'},
+        {name: 'Kota Depok'},
+      ]
+    }
+  }
 };
 </script>
 
 <style scoped>
+.flex-row {
+    display: flex;
+    flex-direction: row;
+}
+
+.flex-col {
+    display: flex;
+    flex-direction: column;
+}
+/* container */
+.container {
+    padding: 40px 76px;
+}
+
+/* header */
+.header {
+    text-align: center;
+}
+
+/* flex container */
+.grid__edit--form {
+    display: grid;
+    grid-template-columns: auto auto auto auto auto auto auto;
+    padding: 10px;
+}
+
+.grid__edit--form > input:active,
+.grid__edit--form > input:focus {
+    outline: none;
+}
+
+.grid__edit--form > label {
+    text-align: center;
+}
+
+/* button */
+.btn__save {
+    float: right;
+    border: 0;
+    background-color: rgba(6, 116, 210, 1);
+    border-radius: 5px;
+    box-shadow: 2px 2px 8px 2px rgba(0, 0, 0, .2);
+    padding: 10px 15px;
+    cursor: pointer;
+    font-weight: 600;
+    margin: 10px 0;
+    color: white;
+}
+
+.btn__save:hover {
+    background-color: rgb(10, 84, 150);
+    transform: scale(1.1);
+}
+
+.col-header {
+    flex: 1 1;
+}
+
+.col-entry {
+    flex: 0 0;
+    flex-basis: 20px;
+}
+
+.row-header {
+    font-weight: 600;
+}
+
+.row-header,
+.row-entry {
+    height: 40px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.row-entry:nth-child(even) {
+    background-color: rgba(0, 0, 0, .05);
+}
+
+.col-header .row-header, .col-header .row-entry {
+    align-items: flex-start;
+}
+
+.number-entry {
+    text-align: center;
+    padding: 5px;
+    margin: 5px;
+    width: 30px;
+    border: 0;
+    border-bottom: 2px solid rgba(0, 0, 0, .5);
+    background-color: transparent;
+}
+
+.number-entry:active,
+.number-entry:focus,
+.number-entry:hover {
+    outline: none;
+    background-color: rgba(6, 116, 210, .05);
+    border-bottom: 2px solid rgba(6, 116, 210, 1);
+}
 
 </style>
