f0beec94e2ce7ac6e95c841ae4fed1cf8409e2c5
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-04-21 09:22:19 +0700

    fix login page

--- viz-dev-frontend/src/views/Login.vue
+++ viz-dev-frontend/src/views/Login.vue
@@ -3,77 +3,94 @@
     <Navbar />
     <div class="d-flex flex-column align-items-center container" >
       <h1 class="title">Login</h1>
-      <form @submit="checkLogin" id="login-form">
+      <div class="login">
         <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" v-model="username"name="username">
+          <label>Username</label>
+          <input type="text" v-model="username">
         </div>
         <div class="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" v-model="password" name="password">
-        </div> 
-        <div class="form-group form-button">
-            <button class="btn login" v-on:click="saveUser">Login</button>
-            <!--<div class="register">
-            	<router-link to="/register" class="btn btn-link">Register</router-link>
-            </div>-->
+          <label>Password</label>
+          <input type="password" v-model="password">
         </div>
-    	</form>
+        <div class="error">{{ this.error }}</div>
+        <div class="d-flex flex-row justify-content-center">
+          <Loader v-if="this.isLoggingIn"/>
+          <button v-if="!this.isLoggingIn" class="btn" v-on:click="login">Login</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import api from '@/api';
+import Loader from '@/components/Loader.vue';
+
 export default {
   name: 'Login',
   components: {
+    Loader,
   },
-
-  methods:{
-    checkLogin: function (e) {
-      if (this.username && this.password) {
-      	return true;
-      }
-
-      this.errors = [];
-
-      if (!this.username) {
-        this.errors.push('Name required.');
-      }
-      if (!this.password) {
-        this.errors.push('Password required.');
-      }
-
-      e.preventDefault();
-    },
-    loginUser: function(){
-    	api.post('/login', {
-	      username: this.username,
-	      password: this.password,
-	    })
+  data() {
+    return {
+      username: '',
+      password: '',
+      isLoggingIn: false,
+      error: '',
+    };
+  },
+  mounted() {
+    const inputs = document.getElementsByTagName('input');
+    for (let i = 0; i < inputs.length; i += 1) {
+      inputs[i].addEventListener('keyup', (e) => {
+        if (e.keyCode === 13) { // enter
+          e.preventDefault();
+          this.login();
+        }
+      });
     }
-  }
+  },
+  methods: {
+    login() {
+      this.isLoggingIn = true;
+      this.error = '';
+      api.post('/login', {
+        username: this.username,
+        password: this.password,
+      }).then(() => {
+        this.isLoggingIn = false;
+        this.$router.push('/edit');
+      }).catch((err) => {
+        this.isLoggingIn = false;
+        this.error = err.response.data.message;
+      });
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 @import '../styles/components/form';
 
-.title {
-    text-align: center;
-    margin: 1.5em 0;
+.login {
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 
-.btn.login {
-    text-align: center;
+.title {
+  text-align: center;
+  margin: 1.5em 0;
 }
 
 .form-button{
-	text-align: center;
+  text-align: center;
 }
 
-.register{
-	margin-top: 5px;
+.error {
+  color: red;
+  font-size: $text-sm;
+  text-align: center;
 }
 </style>
