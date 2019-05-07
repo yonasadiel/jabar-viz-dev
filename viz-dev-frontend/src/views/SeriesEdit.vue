f9e8a78824a1d3b6b58517f725a329e6123a633e
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-05-08 06:05:36 +0700

    fix linting

--- viz-dev-frontend/src/views/SeriesEdit.vue
+++ viz-dev-frontend/src/views/SeriesEdit.vue
@@ -143,10 +143,12 @@ export default {
         this.series = response.data;
       });
     },
-    deleteSeries(seriesId){
+    deleteSeries(seriesId) {
+      /* eslint-disable no-restricted-globals */
+      /* eslint-disable no-alert */
       if (confirm('yakin hapus series ini?')) {
         api.delete(`/series/${seriesId}`).then(() => {
-          this.$router.push("/series")
+          this.$router.push('/series');
         });
       }
     },
@@ -254,7 +256,7 @@ textarea {
 .button-bottom {
   display: flex;
   width: 100%;
-  justify-content: space-between; 
+  justify-content: space-between;
 }
 
 </style>
